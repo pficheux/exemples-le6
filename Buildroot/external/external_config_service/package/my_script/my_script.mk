bcc912b42dee42ffb18c0f922bf8042980dd2451
Pierre Ficheux <pierre.ficheux@smile.fr>
2026-01-02 13:35:39 +0100

    Correction service SysVInit BR

--- Buildroot/external/external_config_service/package/my_script/my_script.mk
+++ Buildroot/external/external_config_service/package/my_script/my_script.mk
@@ -1,7 +1,7 @@
 define MY_SCRIPT_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_SYSVINIT_PATH)/package/my_script/S60my_script  \
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_SERVICE_PATH)/package/my_script/S60my_script  \
 	$(TARGET_DIR)/etc/init.d/S60my_script
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_SYSVINIT_PATH)/package/my_script/my_script.sh \
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_SERVICE_PATH)/package/my_script/my_script.sh \
 	$(TARGET_DIR)/usr/bin
 endef
 
