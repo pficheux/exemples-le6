4192ae740c3c042e3e5973316ceaaf3edf8e561d
Pierre Ficheux <pierre.ficheux@smile.fr>
2026-01-04 18:55:46 +0100

    Ajout custom configs

---
+++ Buildroot/external/external_config_skeleton/custom/pre-build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo
+echo "$(basename $0) -> before the build (id = $(id -u)) !!"
+echo
---
+++ Buildroot/external/external_config_skeleton/custom/pre-rootfs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo
+echo "$(basename $0) -> before creatting the rootfs (id = $(id -u)) !!"
+echo
