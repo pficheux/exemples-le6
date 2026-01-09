3122b5ba9d631a97d4bb5d4374a21376a597c9a9
Pierre Ficheux <pierre.ficheux@smile.fr>
2026-01-09 21:40:47 +0100

    Ajout test dépendance

--- Buildroot/external/external_config_skeleton/package/hello-generic/hello-generic.mk
+++ Buildroot/external/external_config_skeleton/package/hello-generic/hello-generic.mk
@@ -1,6 +1,8 @@
 HELLO_GENERIC_SOURCE = hello-generic.tar.gz
 HELLO_GENERIC_SITE = http://pficheux.free.fr/pub/tmp
 
+HELLO_GENERIC_DEPENDENCIES = simple-dependency
+
 define HELLO_GENERIC_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)
 endef
